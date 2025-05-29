c6cd0eefe79f904802a39156b37263292a3fd760
Faten Eid <87611899+fateneid@users.noreply.github.com>
2025-05-29 03:27:06 +0300

    remove unnecessary lines and comments

--- 05-algorithms-and-problem-solving-level-2.cpp
+++ 05-algorithms-and-problem-solving-level-2.cpp
@@ -1,55 +1,5 @@
 #include <iostream>
 #include "P01_MultiplicationTableFrom1To10.h"
-#include "P02_PrintAllPrimeNumbers1ToN.h"
-#include "P03_PerfectNumber.h"
-#include "P04_PerfectNumberFrom1ToN.h"
-#include "P05_PrintDigitsInAReversedOrder.h"
-#include "P06_SumOfDigits.h"
-#include "P07_ReverseNumber.h"
-#include "P08_DigitFrequency.h"
-#include "P09_DigitFrequency.h"
-#include "P10_PrintDigitsInOrder.h"
-#include "P11_PalindromeNumber.h"
-#include "P12_InvertedNumberPattern.h"
-#include "P13_NumberPattern.h"
-#include "P14_InvertedLetterPattern.h"
-#include "P15_LetterPattern.h"
-#include "P16_AllWordsFromAAAToZZZ.h"
-#include "P17_GuessA3LetterPassword.h"
-#include "P18_EncryptAndDecryptText.h"
-#include "P19_Random3NumbersFrom1To10.h"
-#include "P20_RandomSmallLetterCapitalLetterSpecialCharacterDigit.h"
-#include "P21_GenerateKeys.h"
-#include "P22_RepeatedElementsCountInArray.h"
-#include "P23_FillArrayWithRandomNumbersFrom1To100.h"
-#include "P24_MaxOfRandomArray.h"
-#include "P25_MinOfRandomArray.h"
-#include "P26_SumOfRandomArray.h"
-#include "P27_AverageOfRandomArray.h"
-#include "P28_CopyArray.h"
-#include "P29_CopyOnlyPrimeNumbers.h"
-#include "P30_SumOf2ArraysToAThirdOne.h"
-#include "P31_ShuffleOrderedArray.h"
-#include "P32_CopyArrayInReverseOrder.h"
-#include "P33_FillArrayWithKeys.h"
-#include "P34_ReturnNumberIndexInArray.h"
-#include "P35_CheckNumberInArray.h"
-#include "P36_AddArrayElementSimiDynamic.h"
-#include "P37_CopyArrayUsingAddArrayElement.h"
-#include "P38_CopyOddNumbersToANewArray.h"
-#include "P39_CopyPrimeNumbersToANewArray.h"
-#include "P40_CopyDistinctNumbersToArray.h"
-#include "P41_IsPalindromArray.h"
-#include "P42_CountOddNumbersInArray.h"
-#include "P43_CopyEvenNumbersInArray.h"
-#include "P44_CountPositiveNumbersInArray.h"
-#include "P45_CountNegativeNumbersInArray.h"
-#include "P46_MyABS.h"
-#include "P47_MyRound.h"
-#include "P48_MyFloor.h"
-#include "P49_MyCeil.h"
-#include "P50_MySqrt.h"
-
 
 using namespace std;
 
@@ -57,20 +7,10 @@ using namespace std;
 int main()
 {
 
+
     P01_MultiplicationTableFrom1To10::Task();
 
 
 }
 
-/*
-
-library's namespace name = library name
-
-To run any problem:
-
-    - include its library
-    - use its namespace with any method
-    - run Task fun under its namespace
-
-*/
 
